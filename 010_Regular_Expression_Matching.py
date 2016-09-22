a1d9b38c20ecee3b6ec50b43adf244162b8743e0
Yao Zhang <zhangyao1215@gmail.com>
2016-09-22 15:47:35 -0400

    Create 010_Regular_Expression_Matching.py

---
+++ 010_Regular_Expression_Matching.py
@@ -0,0 +1,73 @@
+"""
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") ? false
+isMatch("aa","aa") ? true
+isMatch("aaa","aa") ? false
+isMatch("aa", "a*") ? true
+isMatch("aa", ".*") ? true
+isMatch("ab", ".*") ? true
+isMatch("aab", "c*a*b") ? true
+"""
+'''
+https://discuss.leetcode.com/topic/48918/a-pattern-based-dp-solution-in-c-best-submission-4ms/4
+Idea: check 
+'''
+
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        return self.matching(s, p)
+        
+    def matching(self, s, p):    
+        slen = len(s)
+        plen = len(p)
+        if plen ==0:
+            if slen == 0:
+                return True
+            else:
+                return False    
+        else: #the len of p is not 0
+            if plen==1: 
+                if p[0] == "*": #check whether p="*"
+                    return True
+                elif slen!=0 and (p[0] == "." or p[0] == s[0]):
+                    return self.matching(s[1:], p[1:])
+                else:    
+                    return False
+            else: # plen >1
+                if p[1]!="*":
+                    if slen!=0 and (p[0] == "." or p[0] == s[0]):
+                        return self.matching(s[1:], p[1:])
+                    else: 
+                        return False
+                else: #p="a* XXX"
+                    if self.matching(s, p[2:]) == True:
+                        return True
+                    else:
+                        if slen!=0 and (p[0] == "." or p[0] == s[0]):
+                            return self.matching(s[1:],p)
+                        else:
+                            return False
+
+def main():
+    a = Solution()
+    if a.isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c") == False:
+        print "False"
+    else:
+        print "True"
+
+if __name__=="__main__":
+    main()
