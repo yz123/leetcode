83b50cf7d8e8204f79ddc18e393e8fbe7227f633
Yao Zhang <zhangyao1215@gmail.com>
2016-09-23 12:04:25 -0400

    Update 010_Regular_Expression_Matching_dp.py

--- 010_Regular_Expression_Matching_dp.py
+++ 010_Regular_Expression_Matching_dp.py
@@ -21,3 +21,81 @@ dp[i+1][j+1]=
    (4) s[: i-1] match p[:j]:  check whether s[i]==p[j-1] (j>=1)
    
 """
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        slen = len(s)
+        plen = len(p)
+        dp = [ [False for j in range(plen+1)] for i in range(slen+1)]
+        
+        #initialize
+        dp[0][0]=True
+        for j in range(plen):
+            if p[j]=="*" and j>=1 and dp[0][j-1]==True:
+                dp[0][j+1]=True
+        
+        for i in range(slen):
+            for j in range(plen):
+                if p[j] != "*":
+                    #if dp[i][j] == True and (p[j]=="." or p[j]==s[i] ):
+                    #    dp[i+1][j+1] = True
+                    dp[i+1][j+1] = ( dp[i][j] and (p[j]=="." or p[j]==s[i] )  )
+                else:
+                    dp[i+1][j+1] = (  dp[i+1][j] or ( j>=1 and dp[i+1][j-1]) or ( j>=1 and dp[i][j+1] and (p[j-1]=="." or p[j-1]==s[i] )) )
+        
+        return dp[slen][plen]
+        
+"""
+class Solution(object):
+    def isMatch(self, s, p):
+        return self.matching(s, p)
+        
+    def matching(self, s, p):    
+        slen = len(s)
+        plen = len(p)
+        if plen ==0:
+            if slen == 0:
+                return True
+            else:
+                return False    
+        else: #the len of p is not 0
+            if plen==1: 
+                if p[0] == "*": #check whether p="*"
+                    return True
+                elif slen!=0 and (p[0] == "." or p[0] == s[0]):
+                    return self.matching(s[1:], p[1:])
+                else:    
+                    return False
+            else: # plen >1
+                if p[1]!="*":
+                    if slen!=0 and (p[0] == "." or p[0] == s[0]):
+                        return self.matching(s[1:], p[1:])
+                    elif slen!=0 and (p[0]=="*"):
+                        return (  self.matching(s[1:], p[1:]) or self.matching(s[1:], p)  )
+                    else: 
+                        return False
+                else: #p="a* XXX"
+                    if self.matching(s, p[2:]) == True:
+                        return True
+                    else:
+                        if slen!=0 and (p[0] == "." or p[0] == s[0]):
+                            return self.matching(s[1:],p)
+                        else:
+                            return False
+"""
+
+def main():
+    a = Solution()
+    #if a.isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c") == False:
+    #if a.isMatch("aab", "c*a*b") == False:
+    if a.isMatch("aaba", "ab*a*c*a") == False:
+        print "False"
+    else:
+        print "True"
+
+if __name__=="__main__":
+    main()
