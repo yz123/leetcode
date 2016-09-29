24b59029b99207e24286261ebc56435fb40177e8
Yao Zhang <zhangyao1215@gmail.com>
2016-09-29 17:51:02 -0400

    Update 132_Palindrome_Partitioning_II_naiveDP.py

--- 132_Palindrome_Partitioning_II_naiveDP.py
+++ 132_Palindrome_Partitioning_II_naiveDP.py
@@ -1,3 +1,12 @@
+"""
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+Return the minimum cuts needed for a palindrome partitioning of s.
+
+For example, given s = "aab",
+Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+"""
+
 # d[i] = min ( i,  min( d[j]+1 [if j+1---i is palindrome ] )  )
 # d[i] = i : initial value
 class Solution(object):
