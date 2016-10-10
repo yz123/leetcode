72e4834d7d584e0a52c7572bd59a7130c770b5aa
Yao Zhang <zhangyao1215@gmail.com>
2016-10-10 19:25:37 -0400

    Create find_lowest_common_ancestor.py

---
+++ find_lowest_common_ancestor.py
@@ -0,0 +1,7 @@
+#http://www.geeksforgeeks.org/lowest-common-ancestor-binary-tree-set-1/
+
+"""
+Method 2 (Using Single Traversal)
+The method 1 finds LCA in O(n) time, but requires three tree traversals plus extra spaces for path arrays. If we assume that the keys n1 and n2 are present in Binary Tree, we can find LCA using single traversal of Binary Tree and without extra storage for path arrays.
+The idea is to traverse the tree starting from root. If any of the given keys (n1 and n2) matches with root, then root is LCA (assuming that both keys are present). If root doesn’t match with any of the keys, we recur for left and right subtree. The node which has one key present in its left subtree and the other key present in right subtree is the LCA. If both keys lie in left subtree, then left subtree has LCA also, otherwise LCA lies in right subtree.
+"""
