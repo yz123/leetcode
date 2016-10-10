be129d2839113afff5693c870ed15bb4763d6515
Yao Zhang <zhangyao1215@gmail.com>
2016-10-10 13:40:33 -0400

    Update 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py

--- 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
+++ 105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -20,7 +20,8 @@ class Solution(object):
         :rtype: TreeNode
         """
         
-        return self.inorder_traverse(preorder, inorder)
+        #return self.inorder_traverse(preorder, inorder)
+        return self.inorder_traverse1(preorder, inorder)
         
         """
         #dfs recursion
@@ -36,6 +37,34 @@ class Solution(object):
         index = self.dic_inorder[root.val]
         root.left = self.dfs(preorder, inorder, pi+1, pi+index-ii, ii, index-1)
         root.right = self.dfs(preorder, inorder, pi+index-ii+1, pj, index+1, ij)
+        return root
+    
+    #myself
+    #https://discuss.leetcode.com/topic/795/the-iterative-solution-is-easier-than-you-think/5
+    def inorder_traverse1(self, preorder, inorder):
+        if len(preorder) == 0: return None
+        stack = []
+        root = TreeNode(preorder[0])
+        stack.append(root)
+        node = root
+        cur_pre = 0
+        cur_in = 0
+        while cur_pre < len(preorder):
+            if preorder[cur_pre] != inorder[cur_in]:
+                node.left = TreeNode(preorder[cur_pre+1])
+                node = node.left
+                stack.append(node)
+            else:
+                #print stack[-1].val, inorder[cur_in]
+                while len(stack)!=0 and stack[-1].val== inorder[cur_in]:
+                    node = stack.pop()
+                    cur_in += 1
+                if cur_pre < len(preorder)-1:
+                    node.right = TreeNode(preorder[cur_pre+1])
+                    node = node.right
+                    stack.append(node)
+            cur_pre += 1
+            #print cur_pre, cur_in
         return root
     
     #https://discuss.leetcode.com/topic/61931/13ms-c-solution-without-recursion
@@ -64,4 +93,3 @@ class Solution(object):
                     node = node.right
                     stack.append(node)
         return root
-    
