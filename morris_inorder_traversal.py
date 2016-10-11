ff2fbc4ff55869d028ea85c1defa9c5ad661e21a
Yao Zhang <zhangyao1215@gmail.com>
2016-10-11 16:54:45 -0400

    Create morris_inorder_traversal.py

---
+++ morris_inorder_traversal.py
@@ -0,0 +1,36 @@
+#http://www.cnblogs.com/AnnieKim/archive/2013/06/15/morristraversal.html
+#idea:
+"""
+如果当前节点左孩子为空，则遍历，否则把当前节点移到其左孩子右子树的最后
+"""
+
+void inorderMorrisTraversal(TreeNode *root) {
+    TreeNode *cur = root, *prev = NULL;
+    while (cur != NULL)
+    {
+        if (cur->left == NULL)          // 1.
+        {
+            printf("%d ", cur->val);
+            cur = cur->right;
+        }
+        else
+        {
+            // find predecessor
+            prev = cur->left;
+            while (prev->right != NULL && prev->right != cur)
+                prev = prev->right;
+
+            if (prev->right == NULL)   // 2.a)
+            {
+                prev->right = cur;
+                cur = cur->left;
+            }
+            else                       // 2.b)
+            {
+                prev->right = NULL;
+                printf("%d ", cur->val);
+                cur = cur->right;
+            }
+        }
+    }
+}
