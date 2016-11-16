d5ba7c74becad1f484e36b4cfa6ff2626d905248
Yao Zhang <zhangyao1215@gmail.com>
2016-11-15 23:33:22 -0500

    Create 049. Group Anagrams.py

---
+++ 049. Group Anagrams.py
@@ -0,0 +1,25 @@
+"""
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note: All inputs will be in lower-case.
+"""
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dict = {}
+        for str in strs:
+            anagram_list = dict.setdefault("".join(sorted(str)), [])
+            anagram_list.append(str)
+        return dict.values()
