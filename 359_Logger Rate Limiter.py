7e0417ca2edc735fe94e9381a57a5ccc91a202a3
Yao Zhang <zhangyao1215@gmail.com>
2017-04-05 01:00:19 -0400

    Update 359_Logger Rate Limiter.py

--- 359_Logger Rate Limiter.py
+++ 359_Logger Rate Limiter.py
@@ -30,3 +30,27 @@ logger.shouldPrintMessage(11,"foo"); returns true;
 Credits:
 Special thanks to @memoryless for adding this problem and creating all test cases.
 """
+
+class Logger(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self._d = {}
+
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity.
+        :type timestamp: int
+        :type message: str
+        :rtype: bool
+        """
+
+        if message in self._d and timestamp - self._d[message] < 10:
+            return False
+        else:
+            self._d[message] = timestamp
+            return True
